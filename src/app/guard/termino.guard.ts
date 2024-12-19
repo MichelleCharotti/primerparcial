d213b5cba0229b6d6043c1c7e4eefcbb48399b02
MichelleCharotti <michellecharotti@gmail.com>
2024-12-19 10:26:28 -0300

    add

--- src/app/guard/termino.guard.ts
+++ src/app/guard/termino.guard.ts
@@ -1,12 +1,13 @@
 import { CanDeactivateFn, Router } from '@angular/router';
 import { UsuarioService } from '../servicios/usuario.service';
 import { inject } from '@angular/core';
+import { TerminosComponent } from '../componentes/terminos/terminos.component';
 
 export const terminoGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
   const usuario=inject(UsuarioService);
   const router=inject(Router);
   
-  if( usuario.obtenerUsuarioTermino() == true)
+  if( usuario.obtenerUsuarioTermino() == true )
 {
   router.navigate(['/bienvenida']);
   return false;
